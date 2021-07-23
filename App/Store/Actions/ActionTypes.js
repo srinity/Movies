1019050addb045461e8dfd45fbe1d9fd028eeef8
Mohamed Gaber <mohamed.elmoazen1@vodafone.com>
2021-07-24 00:03:35 +0200

    [fix] add missing actioon types file

---
+++ App/Store/Actions/ActionTypes.js
@@ -0,0 +1,19 @@
+// Genres List Action Types
+export const GET_GENRE_LIST_STARTED = 'get_genre_list_started'
+export const GET_GENRE_LIST_SUCCESS = 'get_genre_list_success'
+export const GET_GENRE_LIST_FAILED = 'get_genre_list_failed'
+
+// Upcoming Movies Action Types
+export const GET_UPCOMING_MOVIES_STARTED = 'get_upcoming_movies_started'
+export const GET_UPCOMING_MOVIES_SUCCESS = 'get_upcoming_movies_success'
+export const GET_UPCOMING_MOVIES_FAILED = 'get_upcoming_movies_failed'
+
+// Popular Movies Action Types
+export const GET_POPULAR_MOVIES_STARTED = 'get_popular_movies_started'
+export const GET_POPULAR_MOVIES_SUCCESS = 'get_popular_movies_success'
+export const GET_POPULAR_MOVIES_FAILED = 'get_popular_movies_failed'
+
+// Top Rated Movies Action Types
+export const GET_TOP_RATED_MOVIES_STARTED = 'get_top_rated_movies_started'
+export const GET_TOP_RATED_MOVIES_SUCCESS = 'get_top_rated_movies_success'
+export const GET_TOP_RATED_MOVIES_FAILED = 'get_top_rated_movies_failed'
---
+++ App/Store/Actions/index.js
@@ -0,0 +1,5 @@
+import * as ActionTypes from './ActionTypes'
+import * as MoviesActions from './Movies'
+import * as GenresActions from './Genres'
+
+export { ActionTypes, MoviesActions, GenresActions }
