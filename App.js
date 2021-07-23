ba8bbfdd48046c7bc1146e35c11c38495168e36b
Mohamed Gaber <mohamed.elmoazen1@vodafone.com>
2021-07-24 00:04:39 +0200

    [feat] Use redux provider and connect movies screen

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import 'react-native-gesture-handler'
 import React, { Component } from 'react'
+import { ActivityIndicator, View, StyleSheet } from 'react-native'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import AppNavigation from './App/Navigation/AppNavigation'
-import Reactotron from './App/Config/ReactotronConfig'
+import store, { persistor } from './App/Store'
+
+import { Colors } from './App/Theme'
 
 class App extends Component {
+  renderLoading = () => (
+    <View style={styles.indicatorContainerStyle}>
+      <ActivityIndicator color={Colors.brandColor} size="large" />
+    </View>
+  )
+
   render() {
-    return <AppNavigation />
+    return (
+      <Provider store={store}>
+        <PersistGate loading={this.renderLoading()} persistor={persistor}>
+          <AppNavigation />
+        </PersistGate>
+      </Provider>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  indicatorContainerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default App
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -2,18 +2,46 @@ import React, { Component } from 'react'
 import { View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import { MoviesScreen } from '../Screens'
+
+import { MoviesActions } from '../Store/Actions'
 
 const Stack = createStackNavigator()
 
 class AppNavigation extends Component {
+  constructor(props) {
+    super(props)
+
+    this.ConnectedComponents = {
+      Movies: connect(
+        ({ movies, genres }) => {
+          const { genreList } = genres
+
+          return { ...movies, genreList }
+        },
+        dispatch => {
+          return {
+            ...bindActionCreators(MoviesActions, dispatch)
+          }
+        }
+      )(MoviesScreen)
+    }
+  }
+
   render() {
+    const { ConnectedComponents } = this
+
     return (
       <View style={styles.wrapperViewContainerStyle}>
         <NavigationContainer>
           <Stack.Navigator headerMode="none">
-            <Stack.Screen name="Movies" component={MoviesScreen} />
+            <Stack.Screen
+              name="Movies"
+              component={ConnectedComponents.Movies}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
