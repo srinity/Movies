919613b5167c1982d2f809ea6ab713dcc3788c68
Mohamed Gaber <mohamed.elmoazen1@vodafone.com>
2021-07-24 00:05:06 +0200

    [feat] add getUpcomingMovies action call to movies screen

--- App/Screens/Movies/Movies.js
+++ App/Screens/Movies/Movies.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import PropTypes from 'prop-types'
 
 import styles from './Movies.Styles'
 
 class Movies extends Component {
+  componentDidMount() {
+    const { getUpcomingMovies, genreList } = this.props
+    getUpcomingMovies(genreList, 1)
+  }
+
   render() {
+    console.tron.warn(this.props)
     return (
       <View style={styles.containerStyle}>
         <Text>Movies Screen</Text>
       </View>
     )
   }
+}
+
+Movies.propTypes = {
+  getUpcomingMovies: PropTypes.func,
+  getPopularMovies: PropTypes.func,
+  getTopRatedMovies: PropTypes.func,
+  genreList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string
+    })
+  )
 }
 
 export default Movies
