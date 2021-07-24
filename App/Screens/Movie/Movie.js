ff640830d9c4f1388bb77cabceb12ddc913e9154
Mohamed Gaber <mohamed.elmoazen1@vodafone.com>
2021-07-24 23:27:26 +0200

    [feat] Use MovieInfo component in Movie screen

--- App/Screens/Movie/Movie.js
+++ App/Screens/Movie/Movie.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, ScrollView, View, ActivityIndicator } from 'react-native'
 import PropTypes from 'prop-types'
 
-import { Screen } from '../../Components'
+import { Screen, MovieInfo } from '../../Components'
+
+import { Colors } from '../../Theme'
 
 import styles from './Movie.Styles'
 
 class Movie extends Component {
+  componentDidMount() {
+    const {
+      getMovieDetailsAndCredits,
+      route: { params }
+    } = this.props
+
+    getMovieDetailsAndCredits(params.movieId)
+  }
+
   onBackPress = () => {
     const { navigation } = this.props
 
     navigation.pop()
   }
 
+  renderLoading = () => {
+    return (
+      <View style={styles.indicatorContainerStyle}>
+        <ActivityIndicator color={Colors.brandColor} size="large" />
+      </View>
+    )
+  }
+
   render() {
+    const { movieDetails, isMovieDetailsLoading, isMovieCreditsLoading } =
+      this.props
+
     console.tron.warn(this.props)
     return (
       <Screen withBack onBackPress={this.onBackPress}>
-        <Text>Movie Details Screen</Text>
+        {isMovieDetailsLoading || isMovieCreditsLoading ? (
+          this.renderLoading()
+        ) : (
+          <ScrollView style={styles.containerStyle}>
+            <MovieInfo {...movieDetails} />
+          </ScrollView>
+        )}
       </Screen>
     )
   }
@@ -26,7 +54,19 @@ class Movie extends Component {
 Movie.propTypes = {
   navigation: PropTypes.shape({
     pop: PropTypes.func
-  })
+  }),
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      movieId: PropTypes.number
+    })
+  }),
+  getMovieDetailsAndCredits: PropTypes.func,
+  movieDetails: PropTypes.object,
+  isMovieDetailsLoading: PropTypes.bool,
+  movieDetailsError: PropTypes.object,
+  movieCredits: PropTypes.array,
+  isMovieCreditsLoading: PropTypes.bool,
+  movieCreditsError: PropTypes.object
 }
 
 export default Movie
