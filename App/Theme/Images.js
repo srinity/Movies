ea3747c8dbece77ce895abed20179139594fa0d4
Mohamed Gaber <mohamed.elmoazen1@vodafone.com>
2021-07-25 00:20:50 +0200

    [feat] Add dummy avatar image for credits with no poster

--- App/Theme/Images.js
+++ App/Theme/Images.js
@@ -1,4 +1,10 @@
-const Images = {}
+import { Image } from 'react-native'
+
+const Images = {
+  avatar: Image.resolveAssetSource(
+    require('../Assets/avatar-profile-picture.jpeg')
+  )
+}
 
 export default Images
 
